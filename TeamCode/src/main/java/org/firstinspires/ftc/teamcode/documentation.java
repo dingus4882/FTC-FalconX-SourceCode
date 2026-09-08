41ba4005076ac5389878536e3bda3e67563047cf
dingus4882 <dingus4882@gmail.com>
2026-09-08 14:10:06 -0700

    Updated motor reference

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/documentation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/documentation.java
@@ -3,7 +3,7 @@
  *    //-------CONFIGS FOR CONTROLLER--------
  *     //-----CONTROL HUB-----
  *     //---MOTORS---
- *     //GoBUILDA S203
+ *     //GoBUILDA S203 (GOBILDA 5202/3/4)
  *     //0 - FL
  *     //1 - FR
  *     //2 - RL
@@ -11,9 +11,12 @@
  *     //-----EXPANSION HUB-----
  *     //---SERVOS---
  *     //0 - SR2
+ *     - Servo
  *     //---MOTORS---
- *     //0 - INTAKE
+ *     //0 - INTAKE (Unspecified Temporarily)
  *     //1 - GOBILDA 6000 - LN
+ *     - GOBILDA 5202/3/4
  *     //2 - GOBILDA 6000 - LN2
+ *     - GOBILDA 5202/3/4
  *     //3 - REV HEXCORE - SR
  */
